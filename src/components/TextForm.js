61da7f64bfc16e74fc3f08084c9eef1978725a6d
Saurav Panchal <saurav.panchal.08@gmail.com>
2021-09-19 16:12:06 +0530

    modified-TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,25 +37,48 @@ export const TextForm = (props) => {
     const handleClearClick = (event) => {
         let newText = "";
         read = 0;
-        word_count = 0;
-        char_count = 0;
+        // word_count = 0;
+        // char_count = 0;
         document.getElementById("read").innerHTML = read;
-        document.getElementById("word_count").innerHTML = word_count;
-        document.getElementById("char_count").innerHTML = char_count;
+        document.getElementById("word_count").innerHTML = 0;
+        document.getElementById("char_count").innerHTML = 0;
         setText(newText);
     }
-    const handleOnChange = (event, read) => {
-        // console.log("OnChange was click");
-        setText(event.target.value);
-        read = 0.008 * text.split(" ").length;
-        word_count = text.split(" ").length;
-        char_count = text.length+1;
-        // console.log(read);
+
+    const handleCopy = () =>
+    {
+        console.log("I'm Copy.");
+        let tempText = document.getElementById("myBox");
+        tempText.select();
+        navigator.clipboard.writeText(tempText.value);
+    }
+
+    const handleExtraSpaces = () =>
+    {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
+    const handleOnChange = (event, read, word_count, char_count) => {
+        var tempText = event.target.value;
+        var splitTemp = tempText.split(" ");
+        setText(tempText);
+        read = 0.008 * tempText.split(" ").length;
+        char_count = tempText.length;
+        word_count = tempText.split(" ").length;
+        if (splitTemp.at(0) === "")
+        {
+            read = 0;
+            word_count = 0;
+            char_count = 0;
+        }
+        if (splitTemp.at(splitTemp.length-1) === "")
+        {
+            word_count = tempText.split(" ").length-1;
+        }
         document.getElementById("read").innerHTML = read;
         document.getElementById("word_count").innerHTML = word_count;
         document.getElementById("char_count").innerHTML = char_count;
-        // return read;
-        // console.log(text);
     }
 
     const [text, setText] = useState('');
@@ -72,20 +95,13 @@ export const TextForm = (props) => {
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to Lowercase</button>
                 <button className="btn btn-primary mx-2" onClick={handleInverseClick}>Convert to Inversecase</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
                 <button className="btn btn-danger mx-2" onClick={handleClearClick}>Clear</button>
             </div>
             <div className="container my-3">
                 <h2>Your Text Summary</h2>
-                {
-                    // text[text.length] === " "
-                    text.at(-1) !== " " || text.at(0) === " "
-                    ? <p><span id = "word_count">{word_count}</span> words and <span id ="char_count">{char_count}</span> characters</p>
-                    //   console.log("if")
-                    : <p><span id = "word_count">{word_count}</span> words and <span id ="char_count">{char_count}</span> characters</p>
-                    //   console.log("else")
-                    //  console.log("if")
-                    // : console.log("else")
-                }
+                <p><span id = "word_count">{word_count}</span> words and <span id ="char_count">{char_count}</span> characters</p>
                 <p><span id = "read">{read}</span> Minute read</p>
                 <h3>Preview</h3>
                 <p>{text}</p>
